c8fca4f2ff9b216f0eb74e34765a57cb477b91e1
Philip Sanjar <151726769+konnikamii@users.noreply.github.com>
2024-03-03 09:23:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
